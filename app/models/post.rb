470a5f6c7854a556cb396bf5238c7c94562a60f7
MMcClure11 <mcclme02@luther.edu>
2020-07-05 09:44:30 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,38 @@ class Post < ActiveRecord::Base
 
   def click_bait
     if title && !title.include?("Won't Believe"||"Secret"||"Top "||"Guess")
-      errors.add(:title, "Needs to be more exciting!")
+      errors.add(:title, "Your title needs to be more exciting!")
     end
   end
 
+  #########learn.co solution
+  # validate :is_clickbait?
+
+  # CLICKBAIT_PATTERNS = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top [0-9]*/i,
+  #   /Guess/i
+  # ]
+
+  # def is_clickbait?
+  #   if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
+
+  ##########micah solution
+
+#   validate :is_clickbait
+
+#   def is_clickbait
+#     included = false
+#     @@phrases.each { |phrase| included = true if self.title && self.title.downcase =~ phrase }
+#     if not included
+#         errors.add(:title, "You need a more exciting title")
+#     end
+# end
+
+# @@phrases = [/won't believe/, /secret/, /top \d/, /guess/]
 
 end

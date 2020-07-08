f4ba001acb1e860ec24853cf95d5d2c6a4f7f0e5
Haloren <mckennakyler@gmail.com>
2020-07-07 17:14:42 -0700

    Done.

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,24 +1,36 @@
 class PostValidator < ActiveModel::Validator
     
-    # clickbait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-
-    # def validate
-    #     if clickbait.none? { |pat| pat.match title}
-    #         errors.add("This is clickbait")
-    #     end
-    # end
-
-    CLICKBAIT_PATTERNS = [
-        /Won't Believe/i,
-        /Secret/i,
-        /Top [0-9]*/i,
-        /Guess/i
-      ]
-    
-      def is_clickbait?
-        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
-          errors.add(:title, "must be clickbait")
-        end
+  clickbait_strings = [
+    "Won't Believe",
+    "Secret",
+    "Top 0",
+    "Top 1",
+    "Top 2",
+    "Top 3",
+    "Top 4",
+    "Top 5",
+    "Top 6",
+    "Top 7",
+    "Top 8",
+    "Top 9",
+    "Guess"
+  ]
+  def is_clickbait?
+    idx = 0
+    clickbait_exists = false
+    if title
+      while idx < clickbait_string.size
+          if title.index(clickbait_string[idx])
+            clickbait_exists = true
+            break
+          else
+            idx = idx + 1
+          end
       end
-      
+    end
+    if clickbait_exists == false
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end

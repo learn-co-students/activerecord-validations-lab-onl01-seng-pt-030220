0b525771b566ea106e7f2dc0da062e9f450e7846
github username <github email address>
2020-07-13 21:50:23 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,24 @@
 class Post < ActiveRecord::Base
-    validates :title, presence:true
+    validates :title, presence: true
     validates :content, length:{minimum:250}
     validates :summary, length:{maximum:250}
     validates :category, inclusion: ["Fiction", "Non-Fiction"]
     validate :click_baity
  
   def click_baity
-    valid_titles = ["Guess", "Secret", "Won't" "Believe", "Top"]
-    
-  end
+    # titles = self.title
+    valid_titles = ["Guess", "Secret", "Won't Believe", "Top"]
+    valid_titles.each do |a| 
+       if title && title.include?(a) 
+       return true 
+        # else
+        # false 
+        end
+
+      end 
+      errors.add(:title, "must be clickbait")
+  end 
+
 
     # include ActiveModel::Validations
     # validates_with ClickbaitValidator 
